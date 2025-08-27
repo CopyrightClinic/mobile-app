0707492fb6adc66f77c0e7ff818bdc149fd1eb7d
amman-ahmed-brainx <amman.ahmed@brainxtech.com>
2025-08-27 14:52:16 +0500

    PR fixes done

---
+++ lib/core/constants/dimensions.dart
@@ -0,0 +1,47 @@
+class DimensionConstants {
+  // FontSize
+  static const double font2Px = 2.0;
+  static const double font4Px = 4.0;
+  static const double font6Px = 6.0;
+  static const double font8Px = 8.0;
+  static const double font10Px = 10.0;
+  static const double font12Px = 12.0;
+  static const double font14Px = 14.0;
+  static const double font16Px = 16.0;
+  static const double font18Px = 18.0;
+  static const double font20Px = 20.0;
+  static const double font22Px = 22.0;
+  static const double font24Px = 24.0;
+  static const double font26Px = 26.0;
+  static const double font28Px = 28.0;
+  static const double font30Px = 30.0;
+  static const double font32Px = 32.0;
+  static const double font34Px = 34.0;
+  static const double font36Px = 36.0;
+  static const double font38Px = 38.0;
+
+  // Gap
+  static const double gap0Px = 0.0;
+  static const double gap2Px = 2.0;
+  static const double gap4Px = 4.0;
+  static const double gap6Px = 6.0;
+  static const double gap8Px = 8.0;
+  static const double gap10Px = 10.0;
+  static const double gap12Px = 12.0;
+  static const double gap14Px = 14.0;
+  static const double gap15Px = 15.0;
+  static const double gap16Px = 16.0;
+  static const double gap18Px = 18.0;
+  static const double gap20Px = 20.0;
+  static const double gap22Px = 22.0;
+  static const double gap24Px = 24.0;
+  static const double gap26Px = 26.0;
+  static const double gap28Px = 28.0;
+  static const double gap30Px = 30.0;
+  static const double gap32Px = 32.0;
+  static const double gap34Px = 34.0;
+  static const double gap36Px = 36.0;
+  static const double gap38Px = 38.0;
+  static const double gap40Px = 40.0;
+  static const double gap48Px = 48.0;
+}
