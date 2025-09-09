c944cd349ee5b4e683e380a8da2d66560fc21898
amman-ahmed-brainx <amman.ahmed@brainxtech.com>
2025-09-09 18:17:41 +0500

    designed bottombar as per figma

--- lib/core/constants/image_constants.dart
+++ lib/core/constants/image_constants.dart
@@ -4,8 +4,11 @@ class ImageConstants {
   static const String svg = '$base/svg';
   static const String png = '$base/png';
 
-  static const String clients = '$png/clients';
+  // ===================
+  // PNG IMAGES
+  // ===================
 
+  static const String clients = '$png/clients';
   static const String client1 = '$clients/client1.png';
   static const String client2 = '$clients/client2.png';
   static const String client3 = '$clients/client3.png';
@@ -16,16 +19,29 @@ class ImageConstants {
 
   static const String logo = '$png/logo.png';
   static const String splash = '$png/splash.png';
-  static const String aboutUs = '$svg/about_us.svg';
-  static const String whatWeDo = '$svg/what_we_do.svg';
-  static const String askHaroldAi = '$svg/ask_harold_ai.svg';
+
   static const String whatWeDoBG = '$png/what_we_do_bg.png';
+
   static const String musicLicensing = '$png/music.png';
   static const String visualArtistsRights = '$png/visual_arts.png';
   static const String videoAndMedia = '$png/video_media.png';
   static const String artificialIntelligence = '$png/ai.png';
   static const String writers = '$png/writers.png';
-  static const String signupSuccess = '$svg/signup_success.svg';
   static const String snackbarSuccess = '$png/snackbar_success.png';
   static const String snackbarError = '$png/snackbar_error.png';
+
+  // ===================
+  // SVG ICONS
+  // ===================
+
+  static const String aboutUs = '$svg/about_us.svg';
+  static const String whatWeDo = '$svg/what_we_do.svg';
+  static const String askHaroldAi = '$svg/ask_harold_ai.svg';
+  static const String signupSuccess = '$svg/signup_success.svg';
+  static const String homeFill = '$svg/home_filled.svg';
+  static const String sessionsFill = '$svg/session_filled.svg';
+  static const String profileFill = '$svg/profile_filled.svg';
+  static const String home = '$svg/home.svg';
+  static const String sessions = '$svg/session.svg';
+  static const String profile = '$svg/profile.svg';
 }
